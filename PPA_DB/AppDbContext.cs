cd11cd58b1bed8234d3f72aa1adab1b37fe6546d
brwebagency <37336881+brwebagency@users.noreply.github.com>
2021-03-23 01:04:00 +0500

    Update AppDbContext.cs

--- PPA_DB/AppDbContext.cs
+++ PPA_DB/AppDbContext.cs
@@ -14,7 +14,7 @@ namespace PPA_DB {
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseMySql(
-                "server=localhost;user=root;password=89372908085hfvbkm;database=ppa;",
+                "server=localhost;user=root;password=[PASSWORD];database=ppa;",
                 new MySqlServerVersion(new Version(8, 0, 11))
             );
         }
